4f4e83facd7808747acefe6c89aab6b000ec7237
starryeve <958259016@qq.com>
2022-08-10 15:59:30 +0800

    feat: effect 调度执行

--- packages/reactivity/__test__/reactive.test.ts
+++ packages/reactivity/__test__/reactive.test.ts
@@ -1,16 +1,20 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 
 import { reactive, effect } from '../src'
 
 describe('reactivity', () => {
+	beforeEach(() => {
+		vi.useFakeTimers()
+	})
 	let dummy
 	let whoHaveNotSpendBirthday = new Set()
 	let obj = reactive({
 		name: 'rain',
-		age: 20,
+		age: 0,
 		birthdaySpent: false,
 	})
-	it('4.2-基本的响应式', () => {
+
+	it('4.2 基本的响应式', () => {
 		effect(() => {
 			dummy = obj.name
 		})
@@ -19,7 +23,7 @@ describe('reactivity', () => {
 		expect(dummy).toBe('ice')
 	})
 
-	it('4.3-effect 只会在与其建立响应式联系的属性发生变化时执行', () => {
+	it('4.3 effect 只会在与其建立响应式联系的属性发生变化时执行', () => {
 		const fn = vi.fn(() => {
 			dummy = obj.name
 		})
@@ -28,7 +32,7 @@ describe('reactivity', () => {
 		expect(fn).toBeCalledTimes(1)
 	})
 
-	it('4.4-分支切换与 cleanup', () => {
+	it('4.4 分支切换与 cleanup', () => {
 		const fn = vi.fn(() => {
 			if (!obj.birthdaySpent) {
 				// 这个副作用仅在某人的生日还没过时才会执行，也就是说，当生日设置为已度过，副作用不应该执行
@@ -44,7 +48,7 @@ describe('reactivity', () => {
 		expect(fn).toBeCalledTimes(2)
 	})
 
-	it('4.5-嵌套的 effect 与 effect 栈', () => {
+	it('4.5 嵌套的 effect 与 effect 栈', () => {
 		// 想象一下，这两个 render 就代表了两个嵌套组件的渲染函数
 		let outerRender, innerRender
 		const OuterComp = {
@@ -81,7 +85,7 @@ describe('reactivity', () => {
 		expect(innerRender).toHaveBeenCalledTimes(3)
 	})
 
-	it('4-6-避免无限递归循环', () => {
+	it('4.6 避免无限递归循环', () => {
 		let n = 0
 		const fn = () => {
 			obj.age++
@@ -92,5 +96,69 @@ describe('reactivity', () => {
 		effect(fn)
 		obj.age = 0
 		expect(obj.age).toBe(1)
+	})
+
+	it('4.7 调度执行（控制执行顺序）', async () => {
+		const arr1 = [],
+			arr2 = []
+		effect(() => {
+			arr1.push(obj.age)
+		})
+		effect(
+			() => {
+				arr2.push(obj.age)
+			},
+			{
+				scheduler(fn) {
+					setTimeout(fn)
+				},
+			}
+		)
+		obj.age++
+		arr1.push('end')
+		arr2.push('end')
+		await vi.runAllTimers()
+		expect(arr1).toEqual([0, 1, 'end'])
+		expect(arr2).toEqual([0, 'end', 1])
+	})
+
+	it('4.7 调度执行（控制执行次数）', async () => {
+		const jobQueue = new Set()
+		const p = Promise.resolve()
+
+		// 表示是否正在刷新队列
+		let isFlushing = false
+		function flushJob() {
+			// 如果队列正在刷新，则什么都不做
+			if (isFlushing) return
+			// 设置为 true, 代表正在刷新
+			isFlushing = true
+			// 在微任务队列中刷新 jobQueue
+			p.then(() => {
+				jobQueue.forEach((job: any) => job())
+			}).finally(() => {
+				// 结束后重置 isFlushing
+				isFlushing = false
+			})
+		}
+
+		const fn = vi.fn(() => {
+			console.log(obj.age)
+		})
+		effect(fn, {
+			scheduler(fn) {
+				// 每次调度的时候，将副作用函数添加到 jobQueue 队列中
+				jobQueue.add(fn)
+				// 调用 flushJob 刷新队列
+				flushJob()
+			},
+		})
+
+		obj.age++
+		obj.age++
+		obj.age++
+		await vi.runAllTimers()
+		expect(fn).toHaveBeenCalledTimes(2)
+		expect(obj.age).toBe(3)
 	})
 })
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -173,7 +173,7 @@ export function trigger(target: object, key: string | symbol) {
 	effectsToRun.forEach((effectFn: any) => effectFn())
 } */
 
-// 4.6 避免无限递归循环
+/* // 4.6 避免无限递归循环
 let activeEffect
 // effect 栈
 const effectStack: Function[] = []
@@ -237,6 +237,89 @@ export function trigger(target: object, key: string | symbol) {
 	const effectsToRun = new Set(effects)
 	// effectsToRun.forEach((effectFn: any) => effectFn())
 	effectsToRun.forEach((effectFn: any) => {
+		// 如果 trigger 触发执行的副作用函数与当前正在执行的副作用函数相同，则不触发执行
 		if (activeEffect !== effectFn) effectFn()
+	})
+}
+ */
+
+// 4.7 调度执行
+interface EffectOptions {
+	scheduler?: (effectFn: Function) => void
+}
+let activeEffect
+// effect 栈
+const effectStack: Function[] = []
+export function getActiveEffect() {
+	return activeEffect
+}
+
+function cleanup(effectFn) {
+	// 遍历 effectFn.deps 数组
+	for (let i = 0; i < effectFn.deps.length; i++) {
+		// deps 是一个依赖集合
+		const deps = effectFn.deps[i]
+		// 将 effectFn 从依赖集合中移除
+		deps.delete(effectFn)
+	}
+
+	// 因为这里是一个双向的关系，所以还需要充值 effectFn.deps 数组
+	effectFn.deps.length = 0
+}
+export function effect(fn, options: EffectOptions = {}) {
+	const effectFn = () => {
+		cleanup(effectFn)
+		// 当调用 effect 注册副作用函数时，将副作用函数赋值给 activeEffect
+		activeEffect = effectFn
+		// 在调用副作用函数之前将当前的副作用韩式压入栈中
+		effectStack.push(effectFn)
+		fn()
+		// 当前副作用函数执行完毕，将当前副作用函数弹出栈，并把 activeEffect 还原为之前的值
+		effectStack.pop()
+		activeEffect = effectStack[effectStack.length - 1]
+	}
+	// activeEffect.deps 用来存储所有与该副作用函数相关的依赖集合
+	effectFn.deps = []
+	// 将 options 挂载到 effectFn 上
+	effectFn.options = options
+	// 执行副作用函数
+	effectFn()
+}
+const bucket = new WeakMap()
+// 在 get 拦截属性读取时，调用 track 进行依赖追踪
+export function track(target: object, key: string | symbol) {
+	if (!getActiveEffect()) return target[key]
+	let depsMap = bucket.get(target) // key -> effects
+	if (!depsMap) {
+		bucket.set(target, (depsMap = new Map()))
+	}
+	let deps = depsMap.get(key)
+	if (!deps) {
+		depsMap.set(key, (deps = new Set()))
+	}
+	// 将激活的副作用函数添加到“桶”里
+	deps.add(getActiveEffect())
+	// deps 就是一个与当前副作用函数存在联系的依赖集合，将其添加到 activeEffect.deps 数组中
+	activeEffect.deps.push(deps)
+}
+// 在 set 拦截属性设置时，调用 trigger 触发副作用执行
+export function trigger(target: object, key: string | symbol) {
+	const depsMap = bucket.get(target)
+	if (!depsMap) return
+	const effects = depsMap.get(key)
+	// 执行副作用函数
+	// effects && effects.forEach((fn) => fn())
+	const effectsToRun = new Set(effects)
+	// effectsToRun.forEach((effectFn: any) => effectFn())
+	effectsToRun.forEach((effectFn: any) => {
+		// 如果 trigger 触发执行的副作用函数与当前正在执行的副作用函数相同，则不触发执行
+		if (activeEffect !== effectFn) {
+			// 如果一个副作用函数存在调度器，则调用调度器，并将副作用函数作为参数传递
+			if (effectFn.options.scheduler) {
+				effectFn.options.scheduler(effectFn)
+			} else {
+				effectFn()
+			}
+		}
 	})
 }
